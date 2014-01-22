8641070df90c0a3e1f65e0887150b418839054fc
Chris Kitching <chriskitching@linux.com>
2014-01-22 22:09:28 +0000

    Clever finding of target symbols for field accesses.

--- src/main/java/joust/utils/TreeUtils.java
+++ src/main/java/joust/utils/TreeUtils.java
@@ -2,6 +2,7 @@ package joust.utils;
 
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.tree.JCTree;
+import lombok.extern.log4j.Log4j2;
 
 import static com.sun.tools.javac.tree.JCTree.*;
 import static com.sun.tools.javac.code.Symbol.*;
@@ -9,7 +10,8 @@ import static com.sun.tools.javac.code.Symbol.*;
 /**
  * A collection of utility functions for handling JCTree nodes.
  */
-public class TreeUtils {
+public @Log4j2
+class TreeUtils {
     public static boolean isLocalVariable(Symbol sym) {
         return sym.owner instanceof MethodSymbol;
     }
@@ -25,7 +27,17 @@ public class TreeUtils {
 
     public static VarSymbol getTargetSymbolForExpression(JCExpression that) {
         if (that instanceof JCFieldAccess) {
-            return (VarSymbol) ((JCFieldAccess) that).sym;
+            JCFieldAccess cast = (JCFieldAccess) that;
+            while (!(cast.selected instanceof JCIdent)) {
+                if (cast.selected instanceof JCFieldAccess) {
+                    cast = (JCFieldAccess) cast.selected;
+                } else {
+                    log.warn("Unable to find target symbol for {}\nStuck with cast = {}, next {} of type {}", that, cast, cast.selected, cast.selected.getClass().getSimpleName());
+                    return null;
+                }
+            }
+
+            return (VarSymbol) ((JCIdent) cast.selected).sym;
         } else if (that instanceof JCIdent) {
             return (VarSymbol) ((JCIdent) that).sym;
         } else {
