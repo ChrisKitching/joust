b507cfe320eff8e1b25052d81b8b2f6bf27a6933
Chris Kitching <chriskitching@linux.com>
2014-01-25 14:24:02 +0000

    Use static imports to cleanup PTTT.

--- src/main/java/joust/optimisers/translators/ParentTrackingTreeTranslator.java
+++ src/main/java/joust/optimisers/translators/ParentTrackingTreeTranslator.java
@@ -4,6 +4,8 @@ import com.sun.tools.javac.tree.JCTree;
 
 import java.util.LinkedList;
 
+import static com.sun.tools.javac.tree.JCTree.*;
+
 /**
  * A TreeTranslator storing a stack of the nodes between the current node and the root.
  * Allows translators to access the properties of parent nodes of interest.
@@ -14,378 +16,378 @@ public class ParentTrackingTreeTranslator extends BaseTranslator {
     protected LinkedList<JCTree> visitedStack = new LinkedList<>();
 
     @Override
-    public void visitTopLevel(JCTree.JCCompilationUnit jcCompilationUnit) {
+    public void visitTopLevel(JCCompilationUnit jcCompilationUnit) {
         visitedStack.push(jcCompilationUnit);
         super.visitTopLevel(jcCompilationUnit);
         visitedStack.pop();
     }
 
     @Override
-    public void visitImport(JCTree.JCImport jcImport) {
+    public void visitImport(JCImport jcImport) {
         visitedStack.push(jcImport);
         super.visitImport(jcImport);
         visitedStack.pop();
     }
 
     @Override
-    public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
+    public void visitClassDef(JCClassDecl jcClassDecl) {
         visitedStack.push(jcClassDecl);
         super.visitClassDef(jcClassDecl);
         visitedStack.pop();
     }
 
     @Override
-    public void visitMethodDef(JCTree.JCMethodDecl jcMethodDecl) {
+    public void visitMethodDef(JCMethodDecl jcMethodDecl) {
         visitedStack.push(jcMethodDecl);
         super.visitMethodDef(jcMethodDecl);
         visitedStack.pop();
     }
 
     @Override
-    public void visitVarDef(JCTree.JCVariableDecl jcVariableDecl) {
+    public void visitVarDef(JCVariableDecl jcVariableDecl) {
         visitedStack.push(jcVariableDecl);
         super.visitVarDef(jcVariableDecl);
         visitedStack.pop();
     }
 
     @Override
-    public void visitSkip(JCTree.JCSkip jcSkip) {
+    public void visitSkip(JCSkip jcSkip) {
         visitedStack.push(jcSkip);
         super.visitSkip(jcSkip);
         visitedStack.pop();
     }
 
     @Override
-    public void visitBlock(JCTree.JCBlock jcBlock) {
+    public void visitBlock(JCBlock jcBlock) {
         visitedStack.push(jcBlock);
         super.visitBlock(jcBlock);
         visitedStack.pop();
     }
 
     @Override
-    public void visitDoLoop(JCTree.JCDoWhileLoop jcDoWhileLoop) {
+    public void visitDoLoop(JCDoWhileLoop jcDoWhileLoop) {
         visitedStack.push(jcDoWhileLoop);
         super.visitDoLoop(jcDoWhileLoop);
         visitedStack.pop();
     }
 
     @Override
-    public void visitWhileLoop(JCTree.JCWhileLoop jcWhileLoop) {
+    public void visitWhileLoop(JCWhileLoop jcWhileLoop) {
         visitedStack.push(jcWhileLoop);
         super.visitWhileLoop(jcWhileLoop);
         visitedStack.pop();
     }
 
     @Override
-    public void visitForLoop(JCTree.JCForLoop jcForLoop) {
+    public void visitForLoop(JCForLoop jcForLoop) {
         visitedStack.push(jcForLoop);
         super.visitForLoop(jcForLoop);
         visitedStack.pop();
     }
 
     @Override
-    public void visitForeachLoop(JCTree.JCEnhancedForLoop jcEnhancedForLoop) {
+    public void visitForeachLoop(JCEnhancedForLoop jcEnhancedForLoop) {
         visitedStack.push(jcEnhancedForLoop);
         super.visitForeachLoop(jcEnhancedForLoop);
         visitedStack.pop();
     }
 
     @Override
-    public void visitLabelled(JCTree.JCLabeledStatement jcLabeledStatement) {
+    public void visitLabelled(JCLabeledStatement jcLabeledStatement) {
         visitedStack.push(jcLabeledStatement);
         super.visitLabelled(jcLabeledStatement);
         visitedStack.pop();
     }
 
     @Override
-    public void visitSwitch(JCTree.JCSwitch jcSwitch) {
+    public void visitSwitch(JCSwitch jcSwitch) {
         visitedStack.push(jcSwitch);
         super.visitSwitch(jcSwitch);
         visitedStack.pop();
     }
 
     @Override
-    public void visitCase(JCTree.JCCase jcCase) {
+    public void visitCase(JCCase jcCase) {
         visitedStack.push(jcCase);
         super.visitCase(jcCase);
         visitedStack.pop();
     }
 
     @Override
-    public void visitSynchronized(JCTree.JCSynchronized jcSynchronized) {
+    public void visitSynchronized(JCSynchronized jcSynchronized) {
         visitedStack.push(jcSynchronized);
         super.visitSynchronized(jcSynchronized);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTry(JCTree.JCTry jcTry) {
+    public void visitTry(JCTry jcTry) {
         visitedStack.push(jcTry);
         super.visitTry(jcTry);
         visitedStack.pop();
     }
 
     @Override
-    public void visitCatch(JCTree.JCCatch jcCatch) {
+    public void visitCatch(JCCatch jcCatch) {
         visitedStack.push(jcCatch);
         super.visitCatch(jcCatch);
         visitedStack.pop();
     }
 
     @Override
-    public void visitConditional(JCTree.JCConditional jcConditional) {
+    public void visitConditional(JCConditional jcConditional) {
         visitedStack.push(jcConditional);
         super.visitConditional(jcConditional);
         visitedStack.pop();
     }
 
     @Override
-    public void visitIf(JCTree.JCIf jcIf) {
+    public void visitIf(JCIf jcIf) {
         visitedStack.push(jcIf);
         super.visitIf(jcIf);
         visitedStack.pop();
     }
 
     @Override
-    public void visitExec(JCTree.JCExpressionStatement jcExpressionStatement) {
+    public void visitExec(JCExpressionStatement jcExpressionStatement) {
         visitedStack.push(jcExpressionStatement);
         super.visitExec(jcExpressionStatement);
         visitedStack.pop();
     }
 
     @Override
-    public void visitBreak(JCTree.JCBreak jcBreak) {
+    public void visitBreak(JCBreak jcBreak) {
         visitedStack.push(jcBreak);
         super.visitBreak(jcBreak);
         visitedStack.pop();
     }
 
     @Override
-    public void visitContinue(JCTree.JCContinue jcContinue) {
+    public void visitContinue(JCContinue jcContinue) {
         visitedStack.push(jcContinue);
         super.visitContinue(jcContinue);
         visitedStack.pop();
     }
 
     @Override
-    public void visitReturn(JCTree.JCReturn jcReturn) {
+    public void visitReturn(JCReturn jcReturn) {
         visitedStack.push(jcReturn);
         super.visitReturn(jcReturn);
         visitedStack.pop();
     }
 
     @Override
-    public void visitThrow(JCTree.JCThrow jcThrow) {
+    public void visitThrow(JCThrow jcThrow) {
         visitedStack.push(jcThrow);
         super.visitThrow(jcThrow);
         visitedStack.pop();
     }
 
     @Override
-    public void visitAssert(JCTree.JCAssert jcAssert) {
+    public void visitAssert(JCAssert jcAssert) {
         visitedStack.push(jcAssert);
         super.visitAssert(jcAssert);
         visitedStack.pop();
     }
 
     @Override
-    public void visitApply(JCTree.JCMethodInvocation jcMethodInvocation) {
+    public void visitApply(JCMethodInvocation jcMethodInvocation) {
         visitedStack.push(jcMethodInvocation);
         super.visitApply(jcMethodInvocation);
         visitedStack.pop();
     }
 
     @Override
-    public void visitNewClass(JCTree.JCNewClass jcNewClass) {
+    public void visitNewClass(JCNewClass jcNewClass) {
         visitedStack.push(jcNewClass);
         super.visitNewClass(jcNewClass);
         visitedStack.pop();
     }
 
     @Override
-    public void visitLambda(JCTree.JCLambda jcLambda) {
+    public void visitLambda(JCLambda jcLambda) {
         visitedStack.push(jcLambda);
         super.visitLambda(jcLambda);
         visitedStack.pop();
     }
 
     @Override
-    public void visitNewArray(JCTree.JCNewArray jcNewArray) {
+    public void visitNewArray(JCNewArray jcNewArray) {
         visitedStack.push(jcNewArray);
         super.visitNewArray(jcNewArray);
         visitedStack.pop();
     }
 
     @Override
-    public void visitParens(JCTree.JCParens jcParens) {
+    public void visitParens(JCParens jcParens) {
         visitedStack.push(jcParens);
         super.visitParens(jcParens);
         visitedStack.pop();
     }
 
     @Override
-    public void visitAssign(JCTree.JCAssign jcAssign) {
+    public void visitAssign(JCAssign jcAssign) {
         visitedStack.push(jcAssign);
         super.visitAssign(jcAssign);
         visitedStack.pop();
     }
 
     @Override
-    public void visitAssignop(JCTree.JCAssignOp jcAssignOp) {
+    public void visitAssignop(JCAssignOp jcAssignOp) {
         visitedStack.push(jcAssignOp);
         super.visitAssignop(jcAssignOp);
         visitedStack.pop();
     }
 
     @Override
-    public void visitUnary(JCTree.JCUnary jcUnary) {
+    public void visitUnary(JCUnary jcUnary) {
         visitedStack.push(jcUnary);
         super.visitUnary(jcUnary);
         visitedStack.pop();
     }
 
     @Override
-    public void visitBinary(JCTree.JCBinary jcBinary) {
+    public void visitBinary(JCBinary jcBinary) {
         visitedStack.push(jcBinary);
         super.visitBinary(jcBinary);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeCast(JCTree.JCTypeCast jcTypeCast) {
+    public void visitTypeCast(JCTypeCast jcTypeCast) {
         visitedStack.push(jcTypeCast);
         super.visitTypeCast(jcTypeCast);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeTest(JCTree.JCInstanceOf jcInstanceOf) {
+    public void visitTypeTest(JCInstanceOf jcInstanceOf) {
         visitedStack.push(jcInstanceOf);
         super.visitTypeTest(jcInstanceOf);
         visitedStack.pop();
     }
 
     @Override
-    public void visitIndexed(JCTree.JCArrayAccess jcArrayAccess) {
+    public void visitIndexed(JCArrayAccess jcArrayAccess) {
         visitedStack.push(jcArrayAccess);
         super.visitIndexed(jcArrayAccess);
         visitedStack.pop();
     }
 
     @Override
-    public void visitSelect(JCTree.JCFieldAccess jcFieldAccess) {
+    public void visitSelect(JCFieldAccess jcFieldAccess) {
         visitedStack.push(jcFieldAccess);
         super.visitSelect(jcFieldAccess);
         visitedStack.pop();
     }
 
     @Override
-    public void visitReference(JCTree.JCMemberReference jcMemberReference) {
+    public void visitReference(JCMemberReference jcMemberReference) {
         visitedStack.push(jcMemberReference);
         super.visitReference(jcMemberReference);
         visitedStack.pop();
     }
 
     @Override
-    public void visitIdent(JCTree.JCIdent jcIdent) {
+    public void visitIdent(JCIdent jcIdent) {
         visitedStack.push(jcIdent);
         super.visitIdent(jcIdent);
         visitedStack.pop();
     }
 
     @Override
-    public void visitLiteral(JCTree.JCLiteral jcLiteral) {
+    public void visitLiteral(JCLiteral jcLiteral) {
         visitedStack.push(jcLiteral);
         super.visitLiteral(jcLiteral);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeIdent(JCTree.JCPrimitiveTypeTree jcPrimitiveTypeTree) {
+    public void visitTypeIdent(JCPrimitiveTypeTree jcPrimitiveTypeTree) {
         visitedStack.push(jcPrimitiveTypeTree);
         super.visitTypeIdent(jcPrimitiveTypeTree);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeArray(JCTree.JCArrayTypeTree jcArrayTypeTree) {
+    public void visitTypeArray(JCArrayTypeTree jcArrayTypeTree) {
         visitedStack.push(jcArrayTypeTree);
         super.visitTypeArray(jcArrayTypeTree);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeApply(JCTree.JCTypeApply jcTypeApply) {
+    public void visitTypeApply(JCTypeApply jcTypeApply) {
         visitedStack.push(jcTypeApply);
         super.visitTypeApply(jcTypeApply);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeUnion(JCTree.JCTypeUnion jcTypeUnion) {
+    public void visitTypeUnion(JCTypeUnion jcTypeUnion) {
         visitedStack.push(jcTypeUnion);
         super.visitTypeUnion(jcTypeUnion);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeIntersection(JCTree.JCTypeIntersection jcTypeIntersection) {
+    public void visitTypeIntersection(JCTypeIntersection jcTypeIntersection) {
         visitedStack.push(jcTypeIntersection);
         super.visitTypeIntersection(jcTypeIntersection);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeParameter(JCTree.JCTypeParameter jcTypeParameter) {
+    public void visitTypeParameter(JCTypeParameter jcTypeParameter) {
         visitedStack.push(jcTypeParameter);
         super.visitTypeParameter(jcTypeParameter);
         visitedStack.pop();
     }
 
     @Override
-    public void visitWildcard(JCTree.JCWildcard jcWildcard) {
+    public void visitWildcard(JCWildcard jcWildcard) {
         visitedStack.push(jcWildcard);
         super.visitWildcard(jcWildcard);
         visitedStack.pop();
     }
 
     @Override
-    public void visitTypeBoundKind(JCTree.TypeBoundKind typeBoundKind) {
+    public void visitTypeBoundKind(TypeBoundKind typeBoundKind) {
         visitedStack.push(typeBoundKind);
         super.visitTypeBoundKind(typeBoundKind);
         visitedStack.pop();
     }
 
     @Override
-    public void visitErroneous(JCTree.JCErroneous jcErroneous) {
+    public void visitErroneous(JCErroneous jcErroneous) {
         visitedStack.push(jcErroneous);
         super.visitErroneous(jcErroneous);
         visitedStack.pop();
     }
 
     @Override
-    public void visitLetExpr(JCTree.LetExpr letExpr) {
+    public void visitLetExpr(LetExpr letExpr) {
         visitedStack.push(letExpr);
         super.visitLetExpr(letExpr);
         visitedStack.pop();
     }
 
     @Override
-    public void visitModifiers(JCTree.JCModifiers jcModifiers) {
+    public void visitModifiers(JCModifiers jcModifiers) {
         visitedStack.push(jcModifiers);
         super.visitModifiers(jcModifiers);
         visitedStack.pop();
     }
 
     @Override
-    public void visitAnnotation(JCTree.JCAnnotation jcAnnotation) {
+    public void visitAnnotation(JCAnnotation jcAnnotation) {
         visitedStack.push(jcAnnotation);
         super.visitAnnotation(jcAnnotation);
         visitedStack.pop();
     }
 
     @Override
-    public void visitAnnotatedType(JCTree.JCAnnotatedType jcAnnotatedType) {
+    public void visitAnnotatedType(JCAnnotatedType jcAnnotatedType) {
         visitedStack.push(jcAnnotatedType);
         super.visitAnnotatedType(jcAnnotatedType);
         visitedStack.pop();
