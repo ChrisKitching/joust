2cbff7b3ad4b3b6adfeaaca33be277c31ad531bd
Chris Kitching <chriskitching@linux.com>
2014-03-16 18:35:31 +0000

    A test including type parameters.

---
+++ src/test/resources/testinputs/testTypeParams.java
@@ -0,0 +1,40 @@
+package testinputs;
+
+import testutils.BaseIntegrationTestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class testTypeParams extends BaseIntegrationTestCase {
+    public <T extends Collection<Q>, Q> List<Q> merge(List<T> collections) {
+        List<Q> combined = new ArrayList<>();
+        for (T c : collections) {
+            combined.addAll(c);
+        }
+
+        return combined;
+    }
+
+    @Override
+    protected void test() {
+        List<ArrayList<Integer>> inputs = new ArrayList<>();
+
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        inputs.add(a);
+        ArrayList<Integer> b = new ArrayList<>();
+        a.add(4);
+        a.add(5);
+        inputs.add(b);
+        ArrayList<Integer> c = new ArrayList<>();
+        a.add(6);
+        inputs.add(c);
+
+        List<Integer> someInts = merge(inputs);
+        print(Arrays.toString(someInts.toArray()));
+    }
+}
