476184dee54cbc65be28ac99ec580009be506336
Chris Kitching <chriskitching@linux.com>
2014-03-16 18:35:31 +0000

    Update expression normaliser to new tree representation.

--- src/main/java/joust/optimisers/translators/ExpressionNormalisingTranslator.java
+++ src/main/java/joust/optimisers/translators/ExpressionNormalisingTranslator.java
@@ -1,55 +1,23 @@
 package joust.optimisers.translators;
 
-import com.sun.tools.javac.tree.JCTree;
+import joust.tree.annotatedtree.AJCTree;
 import joust.utils.TreeUtils;
 import lombok.extern.log4j.Log4j2;
 
-public @Log4j2 class ExpressionNormalisingTranslator extends BaseTranslator {
+import static joust.tree.annotatedtree.AJCTree.*;
+
+@Log4j2
+public class ExpressionNormalisingTranslator extends BaseTranslator {
     @Override
-    public void visitBinary(JCTree.JCBinary jcBinary) {
-        super.visitBinary(jcBinary);
-        if (TreeUtils.operatorIsCommutative(jcBinary.getTag())) {
-            CommutativitySorter sorter = new CommutativitySorter(jcBinary);
-            log.debug("Commutativity sorter running on: {}", jcBinary);
-            result = sorter.process();
+    public void visitBinary(AJCBinary binary) {
+        super.visitBinary(binary);
+        if (TreeUtils.operatorIsCommutative(binary.getTag())) {
+            CommutativitySorter sorter = new CommutativitySorter(binary);
+            log.debug("Commutativity sorter running on: {}", binary);
+            AJCTree result = sorter.process();
+            binary.swapFor(result);
 
             log.debug("Commutativity sorter returned: {}", result);
         }
-    }
-
-
-    @Override
-    public void visitMethodDef(JCTree.JCMethodDecl tree) {
-        log.info("Unnormalised expression tree:\n {}", tree);
-        super.visitMethodDef(tree);
-        log.info("Normalised expression tree:\n {}", tree);
-    }
-
-    // For SCIENCE
-
-    @Override
-    public void visitIdent(JCTree.JCIdent jcIdent) {
-        log.trace("Visit ident: "+jcIdent);
-        log.trace("Name: "+jcIdent.name);
-        log.trace("Sym: "+jcIdent.sym);
-        log.trace("Sym: "+jcIdent.sym.getClass().getSimpleName());
-        super.visitIdent(jcIdent);
-
-    }
-
-    @Override
-    public void visitSelect(JCTree.JCFieldAccess jcFieldAccess) {
-        log.trace("Visit field access: "+jcFieldAccess);
-        log.trace("selected:" +jcFieldAccess.selected);
-        log.trace("name:" +jcFieldAccess.name);
-        log.trace("sym:" +jcFieldAccess.sym);
-        log.trace("sym:" +jcFieldAccess.sym.getClass().getSimpleName());
-        super.visitSelect(jcFieldAccess);
-    }
-
-    @Override
-    public void visitReference(JCTree.JCMemberReference jcMemberReference) {
-        log.trace("Visit member ref: "+jcMemberReference);
-        super.visitReference(jcMemberReference);
     }
 }
