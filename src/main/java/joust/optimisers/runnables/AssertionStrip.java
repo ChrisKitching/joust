875c30d4105ffb14e9534c08575bf929ce283bf7
Chris Kitching <chriskitching@linux.com>
2014-04-23 00:48:01 +0100

    Log when the assertion stripper starts.

--- src/main/java/joust/optimisers/runnables/AssertionStrip.java
+++ src/main/java/joust/optimisers/runnables/AssertionStrip.java
@@ -2,18 +2,21 @@ package joust.optimisers.runnables;
 
 import com.sun.tools.javac.tree.JCTree;
 import joust.optimisers.translators.AssertionStrippingTranslator;
+import lombok.extern.java.Log;
 
 import static joust.JOUST.conventionalTrees;
 
 /**
  * Assertion stripping.
  */
+@Log
 public class AssertionStrip extends OptimisationRunnable {
     private final AssertionStrippingTranslator stripper = new AssertionStrippingTranslator();
 
     @Override
     public void run() {
         for (JCTree.JCCompilationUnit tree : conventionalTrees) {
+            log.info("Running AssertionStrip.");
             tree.accept(stripper);
         }
     }
