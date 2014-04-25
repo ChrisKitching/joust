96c89b90ae580074f17164c660ca2858a6dbfe83
Chris Kitching <chriskitching@linux.com>
2014-04-26 00:53:49 +0100

    Handle that javac doesn't seek into zipfiles when we want it to.

--- src/main/java/joust/joustcache/ChecksumUtils.java
+++ src/main/java/joust/joustcache/ChecksumUtils.java
@@ -1,12 +1,25 @@
 package joust.joustcache;
 
+import com.sun.tools.javac.file.ZipFileIndexArchive;
+import joust.utils.logging.LogUtils;
+import lombok.experimental.ExtensionMethod;
+import lombok.extern.java.Log;
 import net.jpountz.xxhash.XXHash32;
 import net.jpountz.xxhash.XXHashFactory;
 
 import javax.tools.JavaFileObject;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+@Log
+@ExtensionMethod({Logger.class, LogUtils.LogExtensions.class})
 public class ChecksumUtils {
     private static final int INITIAL_BUFFER_SIZE = 500000;
     private static final int HASH_SEED = 42;
@@ -20,6 +33,9 @@ public class ChecksumUtils {
     private static XXHash32 jniHasher;
     private static XXHash32 unsafeHasher;
 
+    private static Method getPrefixedEntryName;
+    private static Field zipName;
+
     public static void init() {
         if (jniHasher != null) {
             return;
@@ -27,6 +43,17 @@ public class ChecksumUtils {
         // The instances of XXHash32 are thread safe, according to the documentation.
         jniHasher = XXHashFactory.nativeInstance().hash32();
         unsafeHasher = XXHashFactory.unsafeInstance().hash32();
+
+        // Evil magic to work around the javac ZipFile bug...
+        Class<ZipFileIndexArchive.ZipFileIndexFileObject> zClass = ZipFileIndexArchive.ZipFileIndexFileObject.class;
+        try {
+            getPrefixedEntryName = zClass.getDeclaredMethod("getPrefixedEntryName");
+            getPrefixedEntryName.setAccessible(true);
+            zipName = zClass.getDeclaredField("zipName");
+            zipName.setAccessible(true);
+        } catch (NoSuchMethodException | NoSuchFieldException e) {
+            log.fatal("Error starting ChecksumUtils - did javac update?", e);
+        }
     }
 
     /**
@@ -49,11 +76,41 @@ public class ChecksumUtils {
         // The quantity of bytes the last call to read yielded.
         int lastRead = 0;
 
-        InputStream inputStream = classFile.openInputStream();
+        log.info("Hash computation for {}", classFile);
+        long t = System.currentTimeMillis();
+
+        InputStream inputStream;
+
+        // Because javac is stupid...
+        if (classFile instanceof ZipFileIndexArchive.ZipFileIndexFileObject) {
+            // If we find ourselves with one of these, we're unable to actually read it due to a javac bug. (It returns
+            // a negatively-sized ByteArrayInputStream...
+            // Since these happen whenever the class of interest is in a jar, it's far from a rare event, too. Thanks, lads.
+
+            // Best of all, the stuff we need to do it by hand isn't public. Time for some magic.
+
+            String entryName;
+            ZipFile zipFile;
+            try {
+                entryName = (String) getPrefixedEntryName.invoke(classFile);
+                zipFile = new ZipFile((File) zipName.get(classFile));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                log.fatal("Reflective error from ChecksumUtils - did javac update?", e);
+                return -1;
+            }
+
+            ZipEntry conventionalEntry = zipFile.getEntry(entryName);
+
+            inputStream = zipFile.getInputStream(conventionalEntry);
+        } else {
+            inputStream = classFile.openInputStream();
+        }
+
         try {
             while (lastRead != -1) {
                 // Read as many bytes as are currently available into the buffer.
                 lastRead = inputStream.read(buffer, bPointer, buffer.length-bPointer);
+                log.info("Read: {}", lastRead);
                 bPointer += lastRead;
 
                 // If buffer has overflowed, double its size and carry on.
@@ -78,6 +135,8 @@ public class ChecksumUtils {
         } else {
             hash = unsafeHasher.hash(buffer, 0, bPointer, HASH_SEED);
         }
+
+        log.info("Done in {}ms", System.currentTimeMillis() - t);
 
         return hash;
     }
