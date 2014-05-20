33afaea5d68703cc95a947afa3840ac952fc5f0d
Chris Kitching <chriskitching@linux.com>
2014-05-20 17:43:03 +0100

    Interleave constant folding with unroll passes.

--- src/main/java/joust/optimisers/runnables/Unroll.java
+++ src/main/java/joust/optimisers/runnables/Unroll.java
@@ -1,12 +1,13 @@
 package joust.optimisers.runnables;
 
+import joust.optimisers.translators.ConstFoldTranslator;
 import joust.optimisers.unroll.UnrollTranslator;
 
 /**
  * Loop unrolling.
  */
-public class Unroll extends OptimisationRunnable.BluntForce {
+public class Unroll extends OptimisationRunnable.OneTwo {
     public Unroll() {
-        super(UnrollTranslator.class);
+        super(UnrollTranslator.class, ConstFoldTranslator.class);
     }
 }
